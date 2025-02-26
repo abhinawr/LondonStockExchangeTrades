6a82997e7c7dbd5ff1b603aa29163205d24258eb
Abhinaw Ranu <ar.abhinaw@gmail.com>
2025-02-26 13:13:23 +0530

    added unit test project

---
+++ LondonStockExchangeTrades.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LondonStockExchangeTrades.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Add Unit tests for Repositories, Caching, Messaging etc
+        }
+    }
+}
